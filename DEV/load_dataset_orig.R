2fe21cd3b54ac562c6f4caa8bec9ddb5217eef5c
Johan van Tent <tentvanjohan@hotmail.com>
2025-11-01 16:24:48 +0100

    data opgenomen als package data

---
+++ DEV/load_dataset_orig.R
@@ -0,0 +1,27 @@
+# load_data_waterplanten <- function(){
+#   
+#   tabellen <- list.files("data-raw/parquet", full.names = TRUE)
+#   
+#   tabellen <- tabellen[stringr::str_detect(basename(tabellen), "^\\d", negate = TRUE)]
+#   
+#   for (tabelnaam in tabellen){
+#     
+#     tabel_objectnaam <-  
+#       tabelnaam |> 
+#       basename() |> 
+#       stringr::str_remove("\\.parquet") |> 
+#       stringr::str_replace_all(" ", "_") |> 
+#       stringr::str_to_lower() 
+#     
+#     
+#     print(tabel_objectnaam)
+#     tabel <- arrow::open_dataset(tabelnaam) |> dplyr::collect()
+#     
+#     assign(tabel_objectnaam, tabel, envir = .GlobalEnv)
+# 
+# 
+# 
+#   }
+#   
+# }
+
---
+++ data-raw/create_datasets.R
@@ -0,0 +1,36 @@
+tabellen <- list.files("data-raw/parquet", full.names = TRUE)
+
+tabellen <- tabellen[stringr::str_detect(basename(tabellen), "^\\d", negate = TRUE)]
+
+for (tabelnaam in tabellen){
+  
+  tabel_objectnaam <-  
+    tabelnaam |> 
+    basename() |> 
+    stringr::str_remove("\\.parquet") |> 
+    stringr::str_replace_all(" ", "_") |> 
+    stringr::str_to_lower() 
+  
+  
+  print(tabel_objectnaam)
+  tabel <- arrow::open_dataset(tabelnaam) |> dplyr::collect()
+  
+  assign(tabel_objectnaam, tabel, envir = .GlobalEnv)
+  
+  
+  
+}
+
+usethis::use_data(compartments,
+                  datasets,
+                  landscape_data,
+                  landscape_parameters,
+                  locations,
+                  methods,
+                  physicochemical_data,
+                  physicochemical_parameters,
+                  samples,
+                  units,
+                  vegetation_data,
+                  zones,
+                  overwrite = TRUE)
