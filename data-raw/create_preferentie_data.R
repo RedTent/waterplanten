7fae7ee75491b98e5a8f007c9a9928b5aa9f11c9
Johan van Tent <tentvanjohan@hotmail.com>
2026-02-11 16:26:57 +0100

    Eerste 6 preferentietabellen verwerkt

--- data-raw/create_preferentie_data.R
+++ data-raw/create_preferentie_data.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 library(readxl)
-library(janitor)
+# library(janitor)
 
 
 add_grenzen <- function(df) {
@@ -18,13 +18,14 @@ add_grenzen <- function(df) {
   
 } 
 
-verwerk_klassegrenzen <- funntion(df) {
+verwerk_klassegrenzen <- function(df) {
   
   df %>% 
     .[-(1:3),] %>% 
     janitor::row_to_names(1) %>% 
     select(-ind, -`gewogen gemid.`, -optim., -`gemod. chi waarde`) %>% 
     rename(naam = 1, nednaam = 2, n_wateren_met_soort = 3) %>% 
+    filter(!is.na(naam)) %>% 
     pivot_longer(cols = !1:3, names_to = "klasse", values_to = "relatief_voorkomen") %>% 
     mutate(klasse = str_remove_all(klasse, "[[:space:]]")) %>% 
     add_grenzen() %>% 
@@ -34,20 +35,12 @@ verwerk_klassegrenzen <- funntion(df) {
 
 # 1 Alkaliniteit in OW ------------------------------------------------------------
 
-
 alkaliniteit_ow_raw <- read_excel("data-raw/preferenties/Preferentietabel 1 alkaliniteit in oppervlaktewater.xlsx") 
 
 
-alkaliniteit_klassen <- 
+alkaliniteit_ow_klassen <- 
   alkaliniteit_ow_raw %>% 
-  .[-(1:3),] %>% 
-  janitor::row_to_names(1) %>% 
-  select(-ind, -`gewogen gemid.`, -optim., -`gemod. chi waarde`) %>% 
-  rename(naam = 1, nednaam = 2, n_wateren_met_soort = 3) %>% 
-  pivot_longer(cols = 4:9, names_to = "klasse", values_to = "relatief_voorkomen") %>% 
-  mutate(klasse = str_remove_all(klasse, "[[:space:]]")) %>% 
-  add_grenzen() %>% 
-  type_convert() %>% 
+  verwerk_klassegrenzen() %>% 
   mutate(parameter = "alkaliniteit" , eenheid = "umol/l", compartiment = "OW", .before = klasse) 
 
 
@@ -55,49 +48,90 @@ alkaliniteit_klassen <-
 
 kooldioxide_ow_raw <- read_excel("data-raw/preferenties/Preferentietabel 2 kooldioxide (CO2) in oppervlaktewater.xlsx") 
 
-
 kooldioxide_ow_klassen <-
   kooldioxide_ow_raw %>% 
-  .[-(1:3),] %>% 
-  janitor::row_to_names(1) %>% 
-  select(-ind, -`gewogen gemid.`, -optim., -`gemod. chi waarde`) %>% 
-  rename(naam = 1, nednaam = 2, n_wateren_met_soort = 3) %>% 
-  pivot_longer(cols = !1:3, names_to = "klasse", values_to = "relatief_voorkomen") %>% 
-  mutate(klasse = str_remove_all(klasse, "[[:space:]]")) %>% 
-  add_grenzen() %>% 
-  type_convert() %>% 
+  verwerk_klassegrenzen() %>% 
   mutate(parameter = "koolstofdioxide" , eenheid = "umol/l", compartiment = "OW", .before = klasse) 
-  
-
 
 # 3 waterstofcarbonaat in OW ------------------------------------------------
 
 waterstofcarbonaat_ow_raw <- read_excel("data-raw/preferenties/Preferentietabel 3 bicarbonaat oppervlaktewater.xlsx") 
 
-
-kooldioxide_ow_klassen <-
-  kooldioxide_ow_raw %>% 
-  .[-(1:3),] %>% 
-  janitor::row_to_names(1) %>% 
-  select(-ind, -`gewogen gemid.`, -optim., -`gemod. chi waarde`) %>% 
-  rename(naam = 1, nednaam = 2, n_wateren_met_soort = 3) %>% 
-  pivot_longer(cols = !1:3, names_to = "klasse", values_to = "relatief_voorkomen") %>% 
-  mutate(klasse = str_remove_all(klasse, "[[:space:]]")) %>% 
-  add_grenzen() %>% 
-  type_convert() %>% 
-  mutate(parameter = "alkaliniteit" , eenheid = "umol/l", compartiment = "OW", .before = klasse) 
+waterstofcarbonaat_ow_klassen <-
+  waterstofcarbonaat_ow_raw %>% 
+  verwerk_klassegrenzen() %>% 
+  mutate(parameter = "waterstofcarbonaat" , eenheid = "umol/l", compartiment = "OW", .before = klasse) 
 
 
+# 4 pH in OW ----------------------------------------------------------------
+
+ph_ow_raw <- read_excel("data-raw/preferenties/Preferentietabel 4 veld-pH  oppervlaktewater.xlsx") 
+
+ph_ow_klassen <-
+  ph_ow_raw %>% 
+  verwerk_klassegrenzen() %>% 
+  mutate(parameter = "pH" , eenheid = "DIMSLS", compartiment = "OW", .before = klasse) 
+
+
+
+# 5 Totaal fosfor in OW ---------------------------------------------------
+
+totaal_p_ow_raw <- read_excel("data-raw/preferenties/Preferentietabel 5 totaal-fosfor in oppervlaktewater.xlsx") 
+
+totaal_p_ow_klassen <-
+  totaal_p_ow_raw %>% 
+  verwerk_klassegrenzen() %>% 
+  mutate(parameter = "fosfor totaal" , eenheid = "umol/l", compartiment = "OW", .before = klasse) 
+
+
+# 6 Ortho fosfaat in OW ---------------------------------------------------
+
+ortho_p_ow_raw <- read_excel("data-raw/preferenties/Preferentietabel 6 ortho-fosfaat oppervlaktewater.xlsx") 
+
+ortho_p_ow_klassen <-
+  ortho_p_ow_raw %>% 
+  verwerk_klassegrenzen() %>% 
+  mutate(parameter = "ortho-fosfaat" , eenheid = "umol/l", compartiment = "OW", .before = klasse) 
+
+# basis klassen -----------------------------------------------------------
+
+_ow_raw <- read_excel("data-raw/preferenties/") 
+
+_ow_klassen <-
+  _ow_raw %>% 
+  verwerk_klassegrenzen() %>% 
+  mutate(parameter = "" , eenheid = "umol/l", compartiment = "OW", .before = klasse) 
 
 # Testcode ----------------------------------------------------------------
 
-# library(HHSKwkl)
-# theme_set(hhskthema())
-# 
-# soorten <- c("Chara globularis", "Elodea nuttallii", "Ceratophyllum demersum")
-# soorten <- c("Chara globularis", "Utricularia vulgaris", "Lemna minor")
-# 
-# alkaliniteit_klassen %>%
+library(HHSKwkl)
+theme_set(hhskthema())
+
+soorten <- c("Chara globularis", "Elodea nuttallii", "Ceratophyllum demersum")
+soorten <- c("Chara globularis", "Utricularia vulgaris", "Lemna minor")
+
+klassen_combi <-
+  bind_rows(
+    alkaliniteit_klassen,
+    kooldioxide_ow_klassen,
+    waterstofcarbonaat_ow_klassen,
+    ph_ow_klassen,
+    totaal_p_ow_klassen,
+    ortho_p_ow_klassen
+  )
+
+klassen_combi %>%
+  mutate(klasse = fct_reorder(klasse, ondergrens)) %>%
+  filter(naam %in% soorten) %>%
+  group_by(parameter, klasse) %>%
+  summarise(rel_voorkomen = mean(relatief_voorkomen)) %>%
+  ggplot(aes(rel_voorkomen, klasse)) + geom_col() +
+  scale_x_continuous(limits = c(0, NA), expand = expansion(c(0, 0.1))) +
+  HHSKwkl::hhskthema() +
+  facet_wrap(~parameter, scales = "free_y")
+
+
+# alkaliniteit_ow_klassen %>%
 #   mutate(klasse = fct_reorder(klasse, ondergrens)) %>%
 #   filter(naam %in% soorten) %>%
 #   group_by(parameter, klasse) %>%
@@ -105,4 +139,14 @@ kooldioxide_ow_klassen <-
 #   ggplot(aes(rel_voorkomen, klasse)) + geom_col() +
 #   scale_x_continuous(limits = c(0, NA), expand = expansion(c(0, 0.1))) +
 #   HHSKwkl::hhskthema()
+# 
+# kooldioxide_ow_klassen %>%
+#   mutate(klasse = fct_reorder(klasse, ondergrens)) %>%
+#   filter(naam %in% soorten) %>%
+#   group_by(parameter, klasse) %>%
+#   summarise(rel_voorkomen = mean(relatief_voorkomen)) %>%
+#   ggplot(aes(rel_voorkomen, klasse)) + geom_col() +
+#   scale_x_continuous(limits = c(0, NA), expand = expansion(c(0, 0.1))) +
+#   HHSKwkl::hhskthema() +
+#   facet_wrap(~parameter)
 
